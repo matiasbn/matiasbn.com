a0df57b5e8210328485caf01f6f3e91416615bdf
Matías Barrios <matias.barriosn@gmail.com>
2020-05-31 20:01:16 -0400

    resumeLink updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const resumeLink = 'https://drive.google.com/file/d/1lDM4vqUX0f1XhINEpUZ7TzkFKGAjdhzB/view?usp=sharing';
+  const resumeLink =
+    "https://drive.google.com/file/d/1szHiZKK7u-pqogYJb2VJDVU08lF_p--6/view?usp=sharing";
   return (
     <div className="App">
       <header className="App-header">
@@ -15,9 +16,7 @@ function App() {
         </h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          You can see my Resume by clicking
-          {' '}
-          <a href={resumeLink}>here</a>
+          You can see my Resume by clicking <a href={resumeLink}>here</a>
         </p>
       </header>
     </div>
