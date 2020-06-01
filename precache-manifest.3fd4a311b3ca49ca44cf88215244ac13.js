8578fd92a0eec5051dfd77e944fa180fab6ddbb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 00:02:48 +0000

    Deploy www.matiasbn.dev to github.com/matiasbn/matiasbn.dev.git:gh-pages

---
+++ precache-manifest.3fd4a311b3ca49ca44cf88215244ac13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad337772252f8b509fdbfec03c11f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720058579a2bba207ae8",
+    "url": "/static/css/main.cb936c02.chunk.css"
+  },
+  {
+    "revision": "f5103644d74500c81d51",
+    "url": "/static/js/2.a3e2d009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3e2d009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720058579a2bba207ae8",
+    "url": "/static/js/main.276cec54.chunk.js"
+  },
+  {
+    "revision": "8719f5e2bcd324fc7ea1",
+    "url": "/static/js/runtime-main.b0f3f621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
